1d4da0f444ddd47d6c2a372e694faa986b6f1a77
James Milner <jamesmilner22@hotmail.com>
2018-05-30 14:58:42 +0100

    Functional tests

--- tests/functional/index.test.js
+++ tests/functional/index.test.js
@@ -1,17 +1,21 @@
 const { before, describe, it } = intern.getInterface('bdd');
-const { assert } = intern.getPlugin('chai');
+const { assert, expect } = intern.getPlugin('chai');
 
 describe('Index Example', () => {
 
     before(async ({ remote }) => {
-        console.log(remote);
         await remote.get('../../src/index.html');
         await remote.setFindTimeout(5000);
-        await remote.findDisplayedByCssSelector('body.loaded');
+        await remote.findByCssSelector('#functional');
     });
 
     it('should have a div', async ({ remote }) => {
-        const name = await remote.findById('functional');
+        const name = await remote.findByCssSelector('#functional');
+        expect(name).to.not.be.undefined;
+    });
+
+    it('should have text in the div', async ({ remote }) => {
+        const name = await remote.findByCssSelector('#functional');
         const text = await name.getVisibleText()
         assert.strictEqual(text, "I am a div");
     });
